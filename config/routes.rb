d85007f2c01bf2844c6a65400a200f9e551e12f0
OMNIz6 <herookp@gmail.com>
2023-10-01 15:37:50 +0630

    api auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, defaults: { format: :json }, path: '', path_names: {
+
+    sign_in: 'login',
+
+    sign_out: 'logout',
+
+    registration: 'register'
+
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "home" => "home#index"
 end
